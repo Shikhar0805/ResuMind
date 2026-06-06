4b87b7f0a9d1dd57ba33141ed7a1c32cfb0e6504
Shikhar0805 <shikhar0805@gmail.com>
2026-06-06 21:46:43 +0530

    CORS Error Testing

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,30 +1,22 @@
 const express = require('express');
 const app = express();
-const connectToDB=require('../db/db');
+const connectToDB = require('../db/db');
 require('dotenv').config();
-const cookieParser=require('cookie-parser');
-const cors=require('cors');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-const authRoutes=require('../routes/auth.routes');
-const interviewRouter=require('../routes/interview.routes');
+const authRoutes = require('../routes/auth.routes');
+const interviewRouter = require('../routes/interview.routes');
 
 
-const cors = require("cors");
+app.use(cors());
 
-app.use(
-  cors({
-    origin: [
-      "https://resumind-ai-frontend.onrender.com"
-    ],
-    credentials: true,
-  })
-);
 app.use(cookieParser());
-
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.json());
 
 connectToDB();
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/interview', interviewRouter);
