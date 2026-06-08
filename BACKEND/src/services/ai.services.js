25bc038230486df1abed4267a99d0c706cdfa9bc
Shikhar0805 <shikhar0805@gmail.com>
2026-06-09 01:56:10 +0530

    Utilizing 2.5 flash model

--- BACKEND/src/services/ai.services.js
+++ BACKEND/src/services/ai.services.js
@@ -117,7 +117,7 @@ ${selfDescription}
 `;
 
 const response = await ai.models.generateContent({
-  model: "gemini-3.5-flash",
+  model: "gemini-2.5-flash",
   contents: prompt,
   config: {
     responseMimeType: "application/json"
